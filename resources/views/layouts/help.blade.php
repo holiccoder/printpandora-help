d0af17f83d8fe09dcae727a76e2e3fd69a40d3b1
Tycoon INC <sachu@vip.qq.com>
2026-07-24 14:16:01 +0800

    Add global noindex,nofollow robots meta tag

--- resources/views/layouts/help.blade.php
+++ resources/views/layouts/help.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex, nofollow">
     <title>@yield('title', 'Pandoara | Support')</title>
     <link rel="stylesheet" href="{{ asset('css/help.css') }}?v=1">
 </head>
